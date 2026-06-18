1477940f7b76958f4b32e0dde8c9d9bfe5b17189
Arth Sharma <arthsharma@Arths-MacBook-Air-4.local>
2026-06-18 12:28:57 +0530

    Fix: remove duplicate base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/VNM-GNM/',
   plugins: [react()],
-  base: "/VNM-GNM/",
   server: {
     port: 5173,
     open: true
